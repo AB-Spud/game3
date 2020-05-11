295898e21071ebe7f62bcc1b33c876a2ce71be7d
AB-Spud <36827179+AB-Spud@users.noreply.github.com>
2020-05-11 01:02:43 -0500

    fixed gravity related bugs - added physics info

--- game5.py
+++ game5.py
@@ -41,7 +41,7 @@ class PlayerBlock(NonStaticBlock):
 class Game(object):
     def __init__(self, grav, tick_rate):
         self.grav = grav
-        self.accel_speed = 1
+        self.accel_speed = .2
         self.max_accel = -2
         self.drag = .1
         self.running = True
@@ -50,6 +50,8 @@ class Game(object):
 
         self.dt = 0
         self.tf = 0
+
+        self.box_size = 50
 
         self.keys = None
         self.mice = None
@@ -70,7 +72,7 @@ class Game(object):
     
     def get_mouse_block(self):
         x,y = pygame.mouse.get_pos()
-        return pygame.Rect(x-5,y-5,10,10), x,y
+        return pygame.Rect(x-5,y-5,0,0), x,y
     
     def calc_vels(self, x, y, obj): # no use for now
         X = x - obj.x
@@ -94,7 +96,7 @@ class Game(object):
     def reaction_loop(self, rect_list, player):
         for r1 in rect_list:
             self.mbk,x,y = self.get_mouse_block()
-            if r1.contains(self.mbk) and self.mice[1]:
+            if r1.colliderect(self.mbk) and self.mice[1]:
                 r1.move_to(x-r1.width/2,y-r1.height/2)
 
             if not r1.static:
@@ -134,12 +136,12 @@ class Game(object):
             if event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:
                     x,y = pygame.mouse.get_pos()
-                    b = StaticBlock(10, (x-(50/2),y-(50/2),50,50), color=(0,0,0))
+                    b = StaticBlock(10, (x-(self.box_size/2),y-(self.box_size/2),self.box_size,self.box_size), color=(0,0,0))
                     self.blocks.append(b)
                 
                 if event.button == 3:
                     x,y = pygame.mouse.get_pos()
-                    b = NonStaticBlock(.5, (x-(50/2),y-(50/2),50,50), color=(200,55,55))
+                    b = NonStaticBlock(.5, (x-(self.box_size/2),y-(self.box_size/2),self.box_size,self.box_size), color=(200,55,55))
                     self.blocks.append(b)
 
             if event.type == pygame.KEYDOWN:
@@ -153,15 +155,22 @@ class Game(object):
                 if event.key == pygame.K_0:
                     self.grav = -self.grav
                 
+                if event.key == pygame.K_EQUALS:
+                    self.grav+=1
+                
+                if event.key == pygame.K_MINUS:
+                    self.grav-=1
+
                 if event.key == pygame.K_SPACE:
                     if bool(self.jumps):
                         if game.grav > 0:
-                            player.vely = 0
                             player.vely -= 2
-                        else:
-                            player.vely = 0
+                        elif game.grav < 0:
                             player.vely += 2
-                        
+                        else:
+                            pass # nothing interesting happens when gravity is set to 0
+                                 # note : try checking collisions for value to offset here
+                
                         self.jumps-=1
 
                 if event.key == pygame.K_z:
@@ -181,14 +190,22 @@ class Game(object):
             player.velx += self.accel_speed
             if player.velx > -self.max_accel:
                 player.velx = -self.max_accel
+
+        if self.keys[pygame.K_LEFTBRACKET]:
+            if self.box_size > 1:
+                self.box_size-=1
+        
+        if self.keys[pygame.K_RIGHTBRACKET]:
+            self.box_size+=1
         
         if self.mice[0] or self.mice[2]:
             x,y = pygame.mouse.get_pos()
-            pygame.draw.rect(self.level.screen, (0,255,0),[x-(50/2),y-(50/2),50,50])
+            pygame.draw.rect(self.level.screen, (0,255,0),[x-(self.box_size/2),y-(self.box_size/2),self.box_size,self.box_size])
 
     def game_loop(self):
         self.level = Map()
         player = PlayerBlock(1,(500, 250, 10, 10), color=(255,0,0))
+        font = pygame.font.SysFont('Arial', 20)
         self.blocks = self.level.default_map()
         self.blocks.append(player)
         while self.running:
@@ -204,6 +221,19 @@ class Game(object):
             self.reaction_loop(self.blocks, player)
 
             self.draw_rects(self.level.screen, self.blocks)
+
+            xv_txt = font.render(f'x-velocity: {player.velx}', False, (255, 0, 0))
+            yv_txt = font.render(f'y-velocity: {player.vely}', False, (255, 0, 0))
+            grav_txt = font.render(f'gravity: {self.grav}', False, (255, 0, 0))
+            fps_txt = font.render('FPS: {0:.8}'.format(self.clock.get_fps()), False, (255, 0, 0))
+            boxsz_txt = font.render(f"box_size: {self.box_size}", False, (255,0,0))
+
+
+            self.level.screen.blit(boxsz_txt, (890,10))
+            self.level.screen.blit(fps_txt, (20, 20))
+            self.level.screen.blit(yv_txt, (20, 40))
+            self.level.screen.blit(xv_txt, (20, 60))
+            self.level.screen.blit(grav_txt, (20, 80))
 
             pygame.display.flip()
 
